c15a80ca54b21a307c2ece26ca96939d8ee48dbb
Rich Sage <rich.sage@gmail.com>
2017-07-21 14:21:57 +0100

    Finish Laravel ProjectType implementation.

--- src/Graphite/Deployer/ProjectType/Laravel.php
+++ src/Graphite/Deployer/ProjectType/Laravel.php
@@ -2,8 +2,23 @@
 
 namespace Graphite\Deployer\ProjectType;
 
+use Deployer\Deployer;
+use Deployer\Task\Context;
+
 class Laravel implements ProjectType
 {
+    private $recipeDirectory;
+
+    public function setRecipeDirectory($recipeDirectory)
+    {
+        $this->recipeDirectory = $recipeDirectory;
+    }
+
+    public function getSharedFiles()
+    {
+        return [];
+    }
+
     public function getSharedDirectories()
     {
         return ["public/uploads"];
@@ -21,6 +36,58 @@ class Laravel implements ProjectType
 
     public function getDeployTasks()
     {
-        // TODO: Implement getDeployTasks() method.
+        $this->ensureRecipeTasks();
+        $this->registerTasks();
+
+        return [
+            "deploy:prepare",
+            "deploy:lock",
+            "deploy:release",
+            "deploy:update_code",
+            "deploy:copy_dirs",
+            "deploy:shared",
+            "deploy:vendors",
+            "deploy:writable",
+            "graphite:laravel:install_configuration",
+            "artisan:storage:link",
+            "artisan:view:clear",
+            "artisan:cache:clear",
+            "artisan:config:cache",
+            "artisan:optimize",
+            "graphite:laravel:artisan_migrate",
+            "deploy:symlink",
+            "deploy:unlock",
+            "cleanup",
+        ];
+    }
+
+    public function taskInstallConfiguration()
+    {
+        $stage = \Deployer\input()->getArgument("stage");
+        \Deployer\run("cd {{release_path}} && rm -f .env && ln -s .env.{$stage} .env");
+    }
+
+    public function taskArtisanMigrate()
+    {
+        // Only run "artisan:migrate" once (if deploying to multiple servers)
+        static $runOnce = false;
+        if ($runOnce) {
+            return;
+        }
+
+        $runOnce = true;
+        $deployer = Deployer::get();
+        $deployer->tasks->get("artisan:migrate")->run(Context::get());
+    }
+
+    protected function registerTasks()
+    {
+        \Deployer\task("graphite:laravel:install_configuration", [$this, "taskInstallConfiguration"]);
+        \Deployer\task("graphite:laravel:artisan_migrate", [$this, "taskArtisanMigrate"]);
+    }
+
+    private function ensureRecipeTasks()
+    {
+        require_once $this->recipeDirectory . "/laravel.php";
     }
 }
